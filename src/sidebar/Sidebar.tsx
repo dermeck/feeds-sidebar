51466496e66b5ab9640c535ad88ce1ccff6ca47b
Martin <dermeck@posteo.de>
2021-07-10 17:34:51 +0200

    refactoring: extract feed list to separate component

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,10 +1,9 @@
 import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
 import { Button } from "../components/styled";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import feedsSlice, { fetchAllFeedsCommand } from "../store/slices/feeds";
+import { useAppDispatch } from "../store/hooks";
+import { fetchAllFeedsCommand } from "../store/slices/feeds";
 import NewFeedForm from "./NewFeedForm/NewFeedForm";
-import FeedItem from "./FeedItem/FeedItem";
 
 const SidebarContainer = styled.div`
   padding: 0.5rem;
@@ -12,17 +11,7 @@ const SidebarContainer = styled.div`
   color: #38383d;
 `;
 
-const FeedContainer = styled.ul`
-  padding-left: 1rem;
-  margin: 0.3rem 0 0.5rem 0;
-`;
-
-const FeedTitle = styled.label`
-  margin-left: 0.5rem;
-`;
-
 const Sidebar: FunctionComponent = () => {
-  const feeds = useAppSelector((state) => state.feeds.feeds);
   const dispatch = useAppDispatch();
 
   return (
@@ -31,30 +20,6 @@ const Sidebar: FunctionComponent = () => {
       <Button onClick={() => dispatch(fetchAllFeedsCommand())}>
         Fetch Feeds
       </Button>
-      {feeds.map((feed) => (
-        <div key={feed.url}>
-          <FeedTitle>{feed.title || feed.url}</FeedTitle>
-          <FeedContainer>
-            {feed.items.map(
-              (item) =>
-                !item.isRead && (
-                  <FeedItem
-                    key={item.id}
-                    item={item}
-                    onClick={() =>
-                      dispatch(
-                        feedsSlice.actions.itemRead({
-                          feedId: feed.id,
-                          itemId: item.id,
-                        })
-                      )
-                    }
-                  />
-                )
-            )}
-          </FeedContainer>
-        </div>
-      ))}
     </SidebarContainer>
   );
 };
