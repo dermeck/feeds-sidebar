9005a651ca5aefe52c47d224442adb2613d71433
Martin <dermeck@posteo.de>
2021-07-18 16:20:11 +0200

    handle http responses that are Http.OK but not a feed  #32

--- src/store/slices/session.ts
+++ src/store/slices/session.ts
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { addNewFeedByUrl } from './feeds';
+import feedsSlice, { addNewFeedByUrl } from './feeds';
 
 type FeedFetchStatus = 'loading' | 'loaded' | 'error';
 
@@ -15,7 +15,17 @@ export const initialState: SessionSliceState = {
 const sessionSlice = createSlice({
     name: 'session',
     initialState,
-    reducers: {},
+    reducers: {
+        feedParseError(state, action: PayloadAction<string>) {
+            const entry = state.newFeeds.find((x) => x.url === action.payload);
+
+            if (entry !== undefined) {
+                entry.status = 'error';
+            } else {
+                state.newFeeds.push({ url: action.payload, status: 'error' });
+            }
+        },
+    },
     extraReducers: (builder) => {
         builder.addCase(addNewFeedByUrl.pending, (state, action) => {
             const entry = state.newFeeds.find((x) => x.url === action.meta.arg);
@@ -26,13 +36,14 @@ const sessionSlice = createSlice({
                 state.newFeeds.push({ url: action.meta.arg, status: 'loading' });
             }
         });
-        builder.addCase(addNewFeedByUrl.fulfilled, (state, action) => {
-            const entry = state.newFeeds.find((x) => x.url === action.meta.arg);
+        builder.addCase(feedsSlice.actions.addFeed, (state, action) => {
+            // don't act on .fulfilled because the parser could have thrown an error
+            const entry = state.newFeeds.find((x) => x.url === action.payload.url);
 
             if (entry !== undefined) {
                 entry.status = 'loaded';
             } else {
-                state.newFeeds.push({ url: action.meta.arg, status: 'loaded' });
+                state.newFeeds.push({ url: action.payload.url, status: 'loaded' });
             }
         });
         builder.addCase(addNewFeedByUrl.rejected, (state, action) => {
