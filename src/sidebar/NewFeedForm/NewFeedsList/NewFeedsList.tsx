07566cd18db1ff3262c1dccdce7c14a614bd6bfa
Martin <dermeck@posteo.de>
2021-07-19 11:47:20 +0200

    icon indicators for newly aded feeds #32

--- src/sidebar/NewFeedForm/NewFeedsList/NewFeedsList.tsx
+++ src/sidebar/NewFeedForm/NewFeedsList/NewFeedsList.tsx
@@ -1,34 +1,64 @@
+import styled from '@emotion/styled';
 import React, { Fragment, FunctionComponent } from 'react';
+import { AlertTriangle, Check, Loader } from 'react-feather';
 
 import { Label } from '../../../components/styled';
 import { useAppSelector } from '../../../store/hooks';
+
+const List = styled.ul`
+    padding-left: 1rem;
+    margin-top: 0.5rem;
+`;
+
+const ListItem = styled.li`
+    list-style: none;
+    padding: 0.3rem;
+
+    & > svg {
+        vertical-align: middle;
+        margin-right: 0.5rem;
+    }
+`;
 
 const NewFeedsList: FunctionComponent = () => {
     const feeds = useAppSelector((state) => state.feeds.feeds);
     const newFeeds = useAppSelector((state) => state.session.newFeeds);
 
-    // TODO add icons
     return (
         <Fragment>
             <Label>Recently added Feeds:</Label>
-            <ul>
+            <List>
                 {newFeeds.map((newFeed) => {
                     switch (newFeed.status) {
                         case 'loading':
-                            return <li>loading...</li>;
+                            return (
+                                <ListItem>
+                                    <Loader size={16} />
+                                    loading...
+                                </ListItem>
+                            );
                         case 'loaded': {
                             const loadedFeed = feeds.find((f) => f.url === newFeed.url);
                             if (loadedFeed !== undefined) {
-                                return <li>{loadedFeed.title}</li>;
+                                return (
+                                    <ListItem>
+                                        <Check size={16} />
+                                        {loadedFeed.title}
+                                    </ListItem>
+                                );
                             } else {
-                                return <li>moep{newFeed.url}</li>;
+                                return <li>{newFeed.url}</li>;
                             }
                         }
                         case 'error':
-                            return <li>error: {newFeed.url}</li>;
+                            return (
+                                <ListItem>
+                                    <AlertTriangle size={16} /> {newFeed.url}
+                                </ListItem>
+                            );
                     }
                 })}
-            </ul>
+            </List>
         </Fragment>
     );
 };
