f168e3389f8d19d4b1b31c71d7cc19f963634388
Martin <dermeck@posteo.de>
2021-08-02 06:53:59 +0200

    opml import method #38

---
+++ src/services/import.ts
@@ -0,0 +1,36 @@
+import { Feed } from '../store/slices/feeds';
+
+export const readOpmlFile = async (file: File): Promise<ReadonlyArray<Feed>> => {
+    const reader = new FileReader();
+
+    return new Promise((resolve) => {
+        reader.onload = async () => {
+            const fileContent = reader.result as string;
+            resolve(parseXml(fileContent));
+        };
+
+        reader.readAsText(file);
+    });
+};
+
+const parseXml = (fileContent: string): ReadonlyArray<Feed> => {
+    const parsedFeeds: Feed[] = [];
+    const parser = new DOMParser();
+
+    const xml = parser.parseFromString(fileContent, 'application/xml');
+
+    xml.querySelectorAll('outline[xmlUrl]').forEach((feedNode) => {
+        const id = feedNode.getAttribute('text');
+        const url = feedNode.getAttribute('xmlUrl');
+
+        if (id !== null && url !== null) {
+            parsedFeeds.push({
+                id,
+                url,
+                items: [],
+            });
+        }
+    });
+
+    return parsedFeeds;
+};
