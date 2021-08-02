76a82d4637e2d35c05b58d282f6277ea744b5651
Martin <dermeck@posteo.de>
2021-08-02 06:54:43 +0200

    opml export  method #38

---
+++ src/services/export.ts
@@ -0,0 +1,25 @@
+import { Feed } from '../store/slices/feeds';
+
+const exportFilename = 'ytrss-export.xml';
+
+const opmlExport = (feeds: ReadonlyArray<Feed>): void => {
+    const xmlString = `<?xml version="1.0" encoding="UTF-8"?><opml version="1.0"><body>
+    ${feeds.map(createOutlineForFeed).join('\n')}
+    </body></opml>`;
+
+    triggerDownload(new Blob([xmlString], { type: 'text/xml' }));
+};
+
+const createOutlineForFeed = (feed: Feed): string => `<outline text="${feed.title}" xmlUrl="${feed.url}" />`;
+
+const triggerDownload = (content: Blob) => {
+    const link = document.createElement('a');
+
+    link.href = URL.createObjectURL(content);
+    link.download = exportFilename;
+    link.click();
+
+    document.removeChild(link);
+};
+
+export default opmlExport;
