47272d6a17dc3fa389c16426b4edd71e2528a282
Martin <dermeck@posteo.de>
2022-03-19 22:11:37 +0100

    add themes for light and dark

---
+++ src/theme/emotion.d.ts
@@ -0,0 +1,34 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+    export interface Theme {
+        colors: {
+            sidebarBackground: string;
+            sideBarText: string;
+
+            buttonBackgroundColor: string;
+            buttonTextColor: string;
+            buttonHoverBackgroundColor: string;
+            buttonHoverTextColor: string;
+            buttonActiveBackgroundColor: string;
+            buttonActiveTextColor: string;
+
+            toolbarBackgroundColor: string;
+            toolbarTextColor: string;
+            toolbarButtonHoverBackground: string;
+            toolbarButtonActiveBackground: string;
+
+            selectedItemBackgroundColor: string;
+            selectedItemTextColor: string;
+            selectedItemNoFocusBackgroundColor: string;
+            selectedItemNoFocusTextColor: string;
+
+            menuHoverBackgroundColor: string;
+            menuHoverTextColor: string;
+            menuBorderColor: string;
+
+            messageBackgroundColor: string;
+            messageTextColor: string;
+        };
+    }
+}
