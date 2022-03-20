b005684169bdb01f9f3c3ebc3c10ca23a2c8b863
Martin <dermeck@posteo.de>
2022-03-20 20:26:28 +0100

    use theme colors for Button

--- src/base-components/styled/Button.tsx
+++ src/base-components/styled/Button.tsx
@@ -1,39 +1,30 @@
 import styled from '@emotion/styled';
 
-import {
-    buttonActiveBackgroundColor,
-    buttonActiveTextColor,
-    buttonBackgroundColor,
-    buttonHoverBackgroundColor,
-    buttonHoverTextColor,
-    buttonTextColor,
-} from './colors';
-
-export const Button = styled.button`
+export const Button = styled.button<{ active?: boolean }>`
     padding-right: 20px;
     padding-left: 20px;
     border: none;
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
 
-    background-color: ${(props: { active?: boolean }) =>
-        props.active ? buttonActiveBackgroundColor : buttonBackgroundColor};
+    background-color: ${(props) =>
+        props.active ? props.theme.colors.buttonActiveBackgroundColor : props.theme.colors.buttonBackgroundColor};
     border-radius: 4px;
-    color: ${buttonTextColor};
+    color: ${(props) => (props.active ? props.theme.colors.buttonActiveTextColor : props.theme.colors.buttonTextColor)};
     font-size: 13px;
 
     line-height: 27px;
 
     :hover {
-        color: ${buttonHoverTextColor};
-        background-color: ${buttonHoverBackgroundColor};
+        color: ${(props) => props.theme.colors.buttonHoverTextColor};
+        background-color: ${(props) => props.theme.colors.buttonHoverBackgroundColor};
         opacity: 0.9;
     }
 
     :active {
         // button is clicked
-        color: ${buttonActiveTextColor};
-        background-color: ${buttonActiveBackgroundColor};
+        color: ${(props) => props.theme.colors.buttonActiveTextColor};
+        background-color: ${(props) => props.theme.colors.buttonActiveBackgroundColor};
         opacity: 1;
     }
 `;
