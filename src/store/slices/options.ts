e723410c2510e9a38367edf4a8ffcd9665740748
Martin <dermeck@posteo.de>
2022-04-08 19:12:19 +0200

    refactor toggle folders/feed titles to use global state

--- src/store/slices/options.ts
+++ src/store/slices/options.ts
@@ -5,11 +5,13 @@ import { RootState } from '../store';
 export type OptionsSliceState = {
     feedUpdatePeriodInMinutes: number;
     fetchThreadsCount: number;
+    showFeedTitles: boolean;
 };
 
 export const initialState: OptionsSliceState = {
     feedUpdatePeriodInMinutes: 30,
     fetchThreadsCount: 4,
+    showFeedTitles: true,
 };
 
 export const selectOptions = (state: RootState) => state.options;
@@ -20,6 +22,9 @@ const optionsSlice = createSlice({
     reducers: {
         changeFeedUpdatePeriodInMinutes(state, action: PayloadAction<number>) {
             state.feedUpdatePeriodInMinutes = action.payload;
+        },
+        toggleShowFeedTitles(state) {
+            state.showFeedTitles = !state.showFeedTitles;
         },
     },
 });
