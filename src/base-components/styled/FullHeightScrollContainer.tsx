951d92bc168a3d80def62fb22ab3b053549fbd5e
Martin <dermeck@posteo.de>
2022-04-18 20:03:50 +0200

    account for toolbar margin in height of FullHeightScrollContainer

--- src/base-components/styled/FullHeightScrollContainer.tsx
+++ src/base-components/styled/FullHeightScrollContainer.tsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
 
-import { toolbarContainerheight } from './ToolbarContainer';
+import { toolbarContainerheight, toolbarContainerMarginBottom } from './ToolbarContainer';
 
 export const FullHeightScrollContainer = styled.div`
-    height: calc(100vh - ${toolbarContainerheight});
+    height: calc(100vh - ${toolbarContainerheight} - ${toolbarContainerMarginBottom});
     overflow-x: scroll;
 `;
--- src/base-components/styled/ToolbarContainer.tsx
+++ src/base-components/styled/ToolbarContainer.tsx
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled';
 
-export const toolbarContainerheight = '3.6rem';
+export const toolbarContainerheight = '48px';
+export const toolbarContainerMarginBottom = '4px';
 
 export const ToolbarContainer = styled.div`
     display: flex;
     height: ${toolbarContainerheight};
     align-items: center;
-    padding-right: 0.3rem;
-    padding-left: 0.3rem;
-    margin-bottom: 0.8rem;
+    padding-right: 4px;
+    padding-left: 4px;
+    margin-bottom: ${toolbarContainerMarginBottom};
 
     background-color: ${(props) => props.theme.colors.toolbarBackgroundColor};
     color: ${(props) => props.theme.colors.toolbarTextColor};
