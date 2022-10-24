3b056ae079d63892c41d12b7a6accabda8df148c
dermeck <dermeck@posteo.de>
2022-10-24 15:40:54 +0200

    cleanup dragdrop hook

--- src/sidebar/FeedList/dragdrop/useDragDropNode.ts
+++ src/sidebar/FeedList/dragdrop/useDragDropNode.ts
@@ -28,6 +28,22 @@ const calculateRelativeDragDropPosition = (
     return value;
 };
 
+const insertModeByRelativeDropPosition = (relativeDropPosition: RelativeDragDropPosition): InsertMode => {
+    switch (relativeDropPosition) {
+        case RelativeDragDropPosition.Top:
+            return InsertMode.Before;
+
+        case RelativeDragDropPosition.Middle:
+            return InsertMode.Into;
+
+        case RelativeDragDropPosition.Bottom:
+            return InsertMode.After;
+
+        default:
+            throw new UnreachableCaseError(relativeDropPosition);
+    }
+};
+
 const useDragDropNode = (nodeMeta: NodeMeta) => {
     const dispatch = useDispatch();
     const { draggedNode, setDraggedNode } = useContext(DragDropContext);
@@ -36,7 +52,6 @@ const useDragDropNode = (nodeMeta: NodeMeta) => {
     // TODO check if we really need it separately for drop
     const [relativeDropPosition, setRelativDropPosition] = useState<RelativeDragDropPosition | undefined>(undefined);
 
-    const descendentNodeIds = useAppSelector((state) => selectDescendentNodeIds(state.feeds, nodeMeta.nodeId));
     const draggedNodeDescendents = useAppSelector((state) =>
         draggedNode ? selectDescendentNodeIds(state.feeds, draggedNode.nodeId) : [],
     );
@@ -48,31 +63,20 @@ const useDragDropNode = (nodeMeta: NodeMeta) => {
             draggedNode?.nodeType === NodeType.Folder &&
             draggedNode?.nodeId !== undefined);
 
-    const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
-        event.dataTransfer.setData('invalidDroptTargets', descendentNodeIds.join(';'));
-        event.dataTransfer.setData('draggedNodeMeta', JSON.stringify(nodeMeta));
-
+    const handleDragStart = () => {
         if (draggedNode?.nodeId !== nodeMeta.nodeId) {
             setDraggedNode(nodeMeta);
         }
     };
 
     const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-        if (event.dataTransfer.getData('draggedNodeMeta') === '') {
+        if (draggedNode === undefined) {
             // if drag over happens very fast this might not be set properly
             return;
         }
 
-        const invalidDroptTargets = event.dataTransfer.getData('invalidDroptTargets').split(';');
-
-        if (invalidDroptTargets.find((x) => x === nodeMeta.nodeId)) {
-            return;
-        }
-
-        const dragged: NodeMeta = JSON.parse(event.dataTransfer.getData('draggedNodeMeta'));
-
         if (!isDropNotAllowed) {
-            setRelativDropPosition(calculateRelativeDragDropPosition(dragged.nodeType, nodeMeta.nodeType, event));
+            setRelativDropPosition(calculateRelativeDragDropPosition(draggedNode.nodeType, nodeMeta.nodeType, event));
 
             event.preventDefault();
         }
@@ -84,70 +88,30 @@ const useDragDropNode = (nodeMeta: NodeMeta) => {
         }
     };
 
-    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-        const invalidDroptTargets = event.dataTransfer.getData('invalidDroptTargets').split(';');
+    const handleDrop = () => {
+        if (isDropNotAllowed || draggedNode === undefined || relativeDropPosition === undefined) {
+            console.warn(
+                `Could not drop node ${draggedNode} on node ${nodeMeta}, position: ${relativeDragDropPosition}`,
+            );
 
-        if (invalidDroptTargets.find((x) => x === nodeMeta.nodeId)) {
             return;
-        }
-
-        const dragged: NodeMeta = JSON.parse(event.dataTransfer.getData('draggedNodeMeta'));
-
-        // recalculate here, don't rely on local state set in handleDragover
-        // handleDragover is dependend on props.disabled which depends on global store so there can be timing issues
-        // (local) relativeDropPosition can be undefined if drag/drop happens very fast
-        // TODO check if this is still a problem with dragged node in context!
-        const relativeDropPosition = calculateRelativeDragDropPosition(dragged.nodeType, nodeMeta.nodeType, event);
-
-        if (relativeDropPosition === undefined) {
-            throw new Error('Illegal state: relativeDropPosition must be definend when handleDrop is called.');
-        }
-
-        doDrop(event, relativeDropPosition);
-
-        setRelativDropPosition(undefined);
-    };
-
-    const doDrop = (event: React.DragEvent<HTMLDivElement>, relativeDropPosition: RelativeDragDropPosition) => {
-        const draggedNodeMeta: NodeMeta = JSON.parse(event.dataTransfer.getData('draggedNodeMeta'));
-
-        if (!draggedNodeMeta) {
-            // TODO can this actually happen?
-            throw new Error('dragged node must be defined.');
         }
 
         dispatch(
             feedsSlice.actions.moveNode({
-                movedNode: draggedNodeMeta,
+                movedNode: draggedNode,
                 targetNodeId: nodeMeta.nodeId,
-                mode:
-                    nodeMeta.nodeType === NodeType.Folder && draggedNodeMeta.nodeType === NodeType.Feed
-                        ? InsertMode.Into
-                        : insertModeByRelativeDropPosition(relativeDropPosition),
+                mode: insertModeByRelativeDropPosition(relativeDropPosition),
             }),
         );
 
         setDraggedNode(undefined);
-    };
-
-    const insertModeByRelativeDropPosition = (relativeDropPosition: RelativeDragDropPosition): InsertMode => {
-        switch (relativeDropPosition) {
-            case RelativeDragDropPosition.Top:
-                return InsertMode.Before;
-
-            case RelativeDragDropPosition.Middle:
-                return InsertMode.Into;
-
-            case RelativeDragDropPosition.Bottom:
-                return InsertMode.After;
-
-            default:
-                throw new UnreachableCaseError(relativeDropPosition);
-        }
+        setRelativDropPosition(undefined);
     };
 
     const handleDragEnd = () => {
         setDraggedNode(undefined);
+        setRelativDropPosition(undefined);
     };
 
     return {
