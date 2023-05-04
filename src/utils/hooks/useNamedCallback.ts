62211ac44a074c33f023bd63f11f2b02ee9da3e4
dermeck <dermeck@posteo.de>
2023-05-04 17:52:10 +0200

    add TODO for useNamedCallback

--- src/utils/hooks/useNamedCallback.ts
+++ src/utils/hooks/useNamedCallback.ts
@@ -1,5 +1,6 @@
 import { DependencyList, useCallback, useDebugValue } from 'react';
 
+// TODO check if the usages if this hook should be removed (performance impact?)
 // eslint-disable-next-line @typescript-eslint/ban-types
 function useNamedCallback<T extends Function>(name: string, callback: T, deps: DependencyList): T {
     useDebugValue(name);
