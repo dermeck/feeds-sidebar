8efdc54ad35910e95d90a80151ffe09111f5b015
dermeck <dermeck@posteo.de>
2023-07-15 09:51:08 +0200

    comment corrected

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -11,7 +11,7 @@ export const moveOrInsertElementBefore = <T>(array: ReadonlyArray<T>, target: T,
 export const moveOrInsertElementAfter = <T>(array: ReadonlyArray<T>, target: T, moved: T): ReadonlyArray<T> => {
     // remove moved element from current position
     const result = [...array].filter((x) => x !== moved);
-    // insert before target element
+    // insert after target element
     const targetIndex = result.indexOf(target);
     result.splice(targetIndex + 1, 0, moved);
 
