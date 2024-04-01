ee0ffc6fc3d0ffac688a49200cc78cca6d595ad3
dermeck <dermeck@posteo.de>
2024-04-01 09:37:45 +0200

    fix html tag replacement

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,4 +1,9 @@
 export const stripHtmlTags = (html: string) => {
+    // TODO this is not sufficient, see skipped test
+    return html.replace(/<\/?[^>]+(>|$)/g, '');
+    /*
+    // errors in nodejs
     const htmlParser = new DOMParser().parseFromString(html, 'text/html');
     return htmlParser.body.textContent;
+    */
 };
